de08278ca8be22124045c239d020ad8c91fa054b
winggier <winggier@gmail.com>
2022-08-30 02:40:08 -0400

    cell hints

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # import numpy as np
 import os
 from flask import Flask, render_template
-# from pydub import AudioSegment
+from pydub import AudioSegment
 # import flask.cli
 # flask.cli.show_server_banner = lambda *args: None
 
@@ -22,22 +22,22 @@ def home(): return render_template("index.html", data = data)
 
 # <<<<<<<<<<<<<<<<<<<<<  data prepare  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-# def to_clip(f, buf):
-#   for i in clips:
-#     if int(i[0]) == f: tar = i
+def to_clip(f, buf):
+  for i in clips:
+    if int(i[0]) == f: tar = i
 
-#   # # path = "static/1006.wav"
-#   # # path = "static/4092.wav"
-#   path = "static/" + str(f) + ".mp3"
-#   # print(path)
-#   # print(tar)
+  # # path = "static/1006.wav"
+  # # path = "static/4092.wav"
+  path = "static/" + str(f) + ".mp3"
+  # print(path)
+  # print(tar)
 
-#   sound = AudioSegment.from_mp3(path)
+  sound = AudioSegment.from_mp3(path)
 
-#   for idx, x in enumerate(tar[1:]):
-#     outp = "./static/" + tar[0] + '_' + str(idx) + ".mp3"
-#     outa = sound[x[0]-buf:x[1]+buf]
-#     outa.export(outp, format="mp3")
+  for idx, x in enumerate(tar[1:]):
+    outp = "./static/" + tar[0] + '_' + str(idx) + ".mp3"
+    outa = sound[x[0]-buf:x[1]+buf]
+    outa.export(outp, format="mp3")
 
 
 
@@ -129,7 +129,17 @@ data.append(list(set(audio)))
 # for i in audio:
 #   if(len(i) < 5): to_clip(int(i), 2500)
 
-# to_clip(4104, 2500)
+# audio = []
+# for i in files:
+#   if('mp3' in i):
+#     i = i.split('.')[0]
+#     if('_' not in i): to_clip(int(i), 2500)
+    # if('_' not in i): print(i)
+
+# to_clip(4490, 2500)
+
+
+
 
 
 if __name__ == "__main__": app.run(debug=True)
