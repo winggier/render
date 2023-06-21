e1ccdb3eb0ee000b7b5111864e451e918700dea9
yinou23 <yinou@cs.stonybrook.edu>
2023-06-21 01:01:22 -0400

    minors

--- static/appendix.js
+++ static/appendix.js
@@ -127,7 +127,7 @@ function appendix() {
     // news                                                         <<<<<<<<<<<<<<<<<<<<<<<<<<
     else if(appp == 4) {
         html += `<br><div class="info" contenteditable="true" >Jun 20:`;
-        html += `<li class='info'>Bel columns bug fixed</li></div>`;
+        html += `<li class='info'>Bel columns bug fix</li></div>`;
 
         html += `<br><div class="info" contenteditable="true" >May 26:`;
         html += `<li class='info'>Opinion elements added to Bel and CG columns</li></div>`;
